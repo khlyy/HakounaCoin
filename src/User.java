fa11e130f2f2908d3bf4d07c0568709303deb91d
Omar Radwan <omarradwan213@gmail.com>
2018-04-02 12:49:15 +0200

    added User constructor

--- src/User.java
+++ src/User.java
@@ -1,14 +1,28 @@
+import java.security.*;
 import java.util.ArrayList;
 import java.util.TreeSet;
+import java.util.UUID;
 
 public class User {
-	private String userId;
-	private String userName;
-	private long privateKey;
-	private long publicKey;
+	private UUID userId;
+	private String username;
+	private PrivateKey privateKey;
+	private PublicKey publicKey;
 	private ArrayList<User> peers;
 	private ArrayList<Announcement> announcements;
 	private TreeSet<Announcement> announcementSet;
+
+	public User(String username, ArrayList<User> peers) throws NoSuchAlgorithmException {
+		this.userId = UUID.randomUUID();
+		this.username = username;
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("DSA");
+		KeyPair keyPair = kpg.generateKeyPair();
+		this.privateKey = keyPair.getPrivate();
+		this.publicKey = keyPair.getPublic();
+		this.peers = peers;
+		announcements = new ArrayList<>();
+		announcementSet = new TreeSet<>();
+	}
 
 	private void makeTransaction(String transactionId) {
 
